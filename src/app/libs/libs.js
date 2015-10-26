97041d0f8b25c2e6612c16147804e3764971e665
Mathieu Cornic <mathieu.cornic@gmail.com>
2015-10-26 21:18:10 +0100

    Local Pacifico font instead of Google font

--- src/app/libs/libs.js
+++ src/app/libs/libs.js
@@ -4,7 +4,6 @@ function updateBackground(url) {
   var scope = angular.element($("#bg")).scope();
   _.defer(function () {
     scope.$apply(function () {
-      console.log(scope);
       scope.bg = url;
     });
   });
