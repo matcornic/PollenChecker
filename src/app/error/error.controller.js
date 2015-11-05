73f48874b4d47cf6749aef16467a25ae4174c5c9
Mathieu Cornic <mathieu.cornic@gmail.com>
2015-11-05 22:46:24 +0100

    custom 404 error and a little suprise in it

---
+++ src/app/error/error.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+(function () {
+  angular.module('pchk-error').controller('ErrorCtrl', function () {
+    updateBackground('assets/images/rickroll.gif');
+  });
+})();
+
--- src/app/error/error.js
+++ src/app/error/error.js
@@ -1,10 +1,19 @@
 'use strict';
 
 (function () {
-    angular.module('pchk-error', ['pchk-core']).config(function ($stateProvider) {
-        $stateProvider.state('error', {
-            url: '/404',
-            templateUrl: 'app/error/404.html'
-        });
+  angular.module('pchk-error', ['pchk-core', 'pchk-directives']).config(function ($stateProvider) {
+    $stateProvider.state('main.error', {
+      url: '/404',
+      data: {
+        breadcrumb: [],
+        title: '404 Error'
+      },
+      views: {
+        'content@layout': {
+          templateUrl: 'app/error/404.html',
+          controller: 'ErrorCtrl as err'
+        }
+      }
     });
+  });
 })();
